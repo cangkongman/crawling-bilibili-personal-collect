e4efffe05eaa23c56f30ddb7be3170f35def1e22
awslhh <70691271+awslhh@users.noreply.github.com>
2021-05-20 11:19:16 +0800

    Update and rename collect-crawling.py to main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from crawling import crawling
+from crawling import organize
+from viewing import viewing
+
+if __name__ == "__main__":
+    path_collect_data = 'C:\\Users\\86135\\Desktop\\python\\B站个人资料\\收藏夹\\collect-data'  # 存放收藏夹数据的目录
+    crawling(path_collect_data)
+    organize(path_collect_data)
+    viewing(path_collect_data)
