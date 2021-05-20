7bae1ea94d0e540233e609c087cf7e5173a12ac7
awslhh <70691271+awslhh@users.noreply.github.com>
2021-05-20 16:49:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from viewing import organize
 from viewing import viewing
 
 if __name__ == "__main__":
-    path_collect_data = 'C:\\Users\\86135\\Desktop\\python\\B站个人资料\\收藏夹\\collect-data'  # 存放收藏夹数据的目录
+    path_collect_data = ''  # 写入存放收藏夹数据的目录
     crawling(path_collect_data)
     organize(path_collect_data)
     viewing(path_collect_data)
